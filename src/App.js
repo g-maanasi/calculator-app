2b7a49db41e5977340aaf28c335414f65448dfdc
ganapa11 <ganapa11@msu.edu>
2024-04-30 15:31:59 -0500

    Added button functionality and organized buttons

--- src/App.js
+++ src/App.js
@@ -2,28 +2,100 @@ import React from "react";
 import './App.css';
 
 class App extends React.Component {
+
   render() {
+    var firstNum = '';
+    var operator = '';
+    var secondNum = '';
+    var solution = '';
+
     function onButton(event) {
-      const value = event.target.value;
-      alert(value)
+      const buttonVal = event.target.value;
+      
+      // Try to convert the passed value to a number
+      try {
+        let number = Number(buttonVal);
+
+        // If the passed value is an operator
+        if (isNaN(number)) {
+          throw new Error('Not A Number');
+        
+        // If the passed value is a number
+        } else {
+          if (operator.length === 0) {
+            firstNum += buttonVal;
+          } else {
+            secondNum += buttonVal;
+          }
+        }
+      }
+      
+      // Handle button value of an operator
+      catch(err) {
+        if (buttonVal !== '=') {
+          operator = buttonVal;
+
+        } else {
+          // Calculating new expression
+          if (firstNum && secondNum && operator) {
+            if (operator === '+') {
+              solution = Number(firstNum) + Number(secondNum);
+
+            } else if (operator === '-') {
+              solution = Number(firstNum) - Number(secondNum);
+
+            } else if (operator === '*') {
+              solution = Number(firstNum) * Number(secondNum);
+
+            } else if (operator === '/') {
+              // If trying to divide by zero
+              if (secondNum === '0') {
+                solution = 'NaN';
+              } else {
+                solution = Number(firstNum) / Number(secondNum)
+              }
+            }
+          }
+
+          firstNum = '';
+          secondNum = '';
+          operator = '';
+        }
+      }
     }
 
     return (
       <div className="App">
-        <div className="row1">
-          <button type="button" value={1} onClick={e => onButton(e)}>1</button>
-          <button type="button" value={2} onClick={e => onButton(e)}>2</button>
-          <button type="button" value={3} onClick={e => onButton(e)}>3</button>
-        </div>
-        <div className="row2">
-          <button type="button" value={4} onClick={e => onButton(e)}>4</button>
-          <button type="button" value={5} onClick={e => onButton(e)}>5</button>
-          <button type="button" value={6} onClick={e => onButton(e)}>6</button>
-        </div>
-        <div className="row3">
-          <button type="button" value={7} onClick={e => onButton(e)}>7</button>
-          <button type="button" value={8} onClick={e => onButton(e)}>8</button>
-          <button type="button" value={9} onClick={e => onButton(e)}>9</button>
+
+        <div id="calculator">
+          <div id="numberPad">
+            <div className="row1">
+              <button type="button" value={1} onClick={e => onButton(e)}>1</button>
+              <button type="button" value={2} onClick={e => onButton(e)}>2</button>
+              <button type="button" value={3} onClick={e => onButton(e)}>3</button>
+            </div>
+            <div className="row2">
+              <button type="button" value={4} onClick={e => onButton(e)}>4</button>
+              <button type="button" value={5} onClick={e => onButton(e)}>5</button>
+              <button type="button" value={6} onClick={e => onButton(e)}>6</button>
+            </div>
+            <div className="row3">
+              <button type="button" value={7} onClick={e => onButton(e)}>7</button>
+              <button type="button" value={8} onClick={e => onButton(e)}>8</button>
+              <button type="button" value={9} onClick={e => onButton(e)}>9</button>
+            </div>
+            <div className="row4">
+              <button type="button" value={0} onClick={e => onButton(e)}>0</button>
+            </div>
+          </div>
+
+          <div id='operator-buttons'>
+            <button type="button" value={'+'} onClick={e => onButton(e)}>+</button>
+            <button type="button" value={'-'} onClick={e => onButton(e)}>-</button>
+            <button type="button" value={'*'} onClick={e => onButton(e)}>*</button>
+            <button type="button" value={'/'} onClick={e => onButton(e)}>/</button>
+            <button type="button" value={'='} onClick={e => onButton(e)}>=</button>
+          </div>
         </div>
       </div>
     );
